828b9df6e4ffdbb53b2f231f1ebd5256171408f3
Sam Nicholls <sam.n@studio8media.co.uk>
2016-05-01 03:12:17 +0100

    dodgy load-db script

---
+++ load-db.py
@@ -0,0 +1,17 @@
+import app
+import os
+
+app.db.create_all()
+
+lh = app.User("localhost")
+app.db.session.add(lh)
+
+for f in os.listdir('webms/all'):
+    video = app.Video(f, f.replace(".webm", ""))
+    app.db.session.add(video)
+app.db.session.commit()
+
+ip = app.Address("127.0.0.1", lh)
+app.db.session.add(ip)
+app.db.session.commit()
+
